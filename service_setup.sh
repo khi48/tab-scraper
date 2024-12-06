ab5ef2deab775f91042120e1f94f412ef1e8ef01
Kieran <kieranhitchcock28@gmail.com>
2024-12-06 15:50:52 +1300

    updating enviroment stuff

--- service_setup.sh
+++ service_setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 SERVICE_NAME="docker-compose-mongodb.service"
 
+# not sure if this will fail on fresh installation
+sudo systemctl stop $SERVICE_NAME
+sudo systemctl disable $SERVICE_NAME
+sudo rm /etc/systemd/system/$SERVICE_NAME
+
 sudo cp ./$SERVICE_NAME /etc/systemd/system/
 sudo sudo systemctl daemon-reload
 sudo systemctl enable $SERVICE_NAME
 sudo systemctl start $SERVICE_NAME 
-sudo systemctl status $SERVICE_NAME 
+sudo systemctl status $SERVICE_NAME
